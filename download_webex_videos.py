7957d10a560769a38a66ad49daf150c02348e856
Ryan Burns <ryanmburns93@gmail.com>
2021-09-15 09:07:54 -0600

    Create utilities.py and consolidate common functions

--- download_webex_videos.py
+++ download_webex_videos.py
@@ -19,6 +19,7 @@ from datetime import timezone, timedelta
 from pathlib import Path
 import os
 import sys
+from utilities import go_to_downloads, launch_webdriver
 
 
 url_xpath_dict = {'url': ('https://gartner.webex.com/webappng/sites'
@@ -34,36 +35,6 @@ url_xpath_dict = {'url': ('https://gartner.webex.com/webappng/sites'
                                            'div/div[1]/i[1]'),
                   'download_button_loc2': ('/html/body/div[1]/div[4]/div/div/'
                                            'div/div[1]/i[1]')}
-
-
-def launch_webdriver(webdriver_path=None):
-    """
-    Instantiate the webdriver application.
-
-    Need to save updated webdriver application to downloads folder or
-    hardcode path to webdriver. Options can also be set to run a
-    headless webdriver, but this is not the given state due to login
-    requirements.
-
-    Parameters
-    ----------
-    webdriver_path : string, optional
-        Alternate filepath to directory containing chromedriver.exe.
-        The default is None.
-
-    Returns
-    -------
-    driver : webdriver
-        Initialized webdriver for retrieving and manipulating web pages.
-    """
-    options = Options()
-    if webdriver_path is None:
-        go_to_downloads()
-    driver_filepath = (os.path.join(os.getcwd(), 'chromedriver.exe'))
-    sys.path.append(driver_filepath)
-    driver = webdriver.Chrome(driver_filepath,
-                              options=options)
-    return driver
 
 
 def get_webex_video_list(url, api_url, driver):
@@ -221,73 +192,6 @@ def utc_to_local(utc_dt):
     fmt = '%m-%d-%Y, %I.%M %p'
     vid_dt = utc_dt.astimezone(local_tz)
     return vid_dt.strftime(fmt)
-
-
-def go_to_downloads():
-    """
-    Navigate to downloads folder regardless of OS.
-
-    This function is a modified version of the code from
-    Michael Kropat at https://gist.github.com/mkropat/7550097.
-
-    Returns
-    -------
-    string
-        Path to the new working directory (which should be the
-        Downloads folder).
-
-    """
-    if os.name == 'nt':
-        import ctypes
-        from ctypes import windll, wintypes
-        from uuid import UUID
-
-        # ctypes GUID copied from MSDN sample code
-        class GUID(ctypes.Structure):
-            _fields_ = [
-                ("Data1", wintypes.DWORD),
-                ("Data2", wintypes.WORD),
-                ("Data3", wintypes.WORD),
-                ("Data4", wintypes.BYTE * 8)
-            ]
-
-            def __init__(self, uuidstr):
-                uuid = UUID(uuidstr)
-                ctypes.Structure.__init__(self)
-                self.Data1, self.Data2, self.Data3, \
-                    self.Data4[0], self.Data4[1], rest = uuid.fields
-                for i in range(2, 8):
-                    self.Data4[i] = rest >> (8-i-1)*8 & 0xff
-
-        SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
-        SHGetKnownFolderPath.argtypes = [
-            ctypes.POINTER(GUID), wintypes.DWORD,
-            wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
-        ]
-
-        def _get_known_folder_path(uuidstr):
-            pathptr = ctypes.c_wchar_p()
-            guid = GUID(uuidstr)
-            if SHGetKnownFolderPath(ctypes.byref(guid),
-                                    0,
-                                    0,
-                                    ctypes.byref(pathptr)):
-                raise ctypes.WinError()
-            return pathptr.value
-
-        FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
-
-        def get_download_folder():
-            new_wd = _get_known_folder_path(FOLDERID_Download)
-            os.chdir(new_wd)
-            return new_wd
-    else:
-        def get_download_folder():
-            home = os.path.expanduser("~")
-            new_wd = os.path.join(home, "Downloads")
-            os.chdir(new_wd)
-            return new_wd
-    get_download_folder()
 
 
 def get_date_val():
---
+++ vtt_caption_parsing.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 13:29:19 2021
+
+@author: Ryan
+"""
+
+import webvtt
+from pathlib import Path
+import os
+from utilities import go_to_downloads
+
+text_lst = []
+transcript = []
+
+
+def read_caption(webvtt_file):
+    text_lst = []
+    for caption in webvtt.read(webvtt_file):
+        text_lst.append(caption.text)
+    return text_lst
+
+
+def get_vtt_file_list():
+    go_to_downloads()
+    path = Path('.')
+    video_list = list(path.glob('**/*.vtt'))
+    return video_list
+
+
+def main():
+    video_list = get_vtt_file_list()
+    i = 0
+    num_vids = len(video_list)
+    for video in video_list:
+        transcript = read_caption(video)
+        text = ' '.join(transcript).replace('\n',' ')
+        filestring = video.name[:-4] + '.txt'
+        with open(filestring, "w+", encoding='utf-8') as file:
+            file.write(text)
+        i += 1
+        print(f'{round(i/num_vids*100, 2)}% complete')
+
+
+if __name__ == "__main__":
+    main()
